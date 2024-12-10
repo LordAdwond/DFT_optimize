f75729ea8d770b2f28a18d87ce747603a42b9054
LordAdwond <andreymakarh2@gmail.com>
2024-12-11 01:53:32 +0300

    Version with comparative test

--- DFT.cs
+++ DFT.cs
@@ -8,69 +8,118 @@ namespace DFT_optimize
 {
     class DFT
     {
-        static public Complex[] Calc_DFT(double[] arr)
+        static public Complex[] Calc_DFT(double[] arr, int l, int a=0)
         {
-            int q = (int)Math.Log(arr.Length, 2);
-            int N = (int)Math.Pow(2, q);
+            int N = l;
+            double[] data = new double[l];
             Complex[] dft = new Complex[N];
             int n = 0, k = 0;
             double pi = Math.PI;
             double Real = 0, Imag = 0;
-            
-            for (n = 0; n < N; n++)
-            {
-                Real = Imag = 0;
-                
-                dft[n] = new Complex(0, 0);
 
-                for(k = 0; k < N; k++)
+            for(int i=0; i<l; i++)
+            {
+                data[i] = arr[i + a + l];
+            }
+            
+            var ForDFT = Parallel.For(0, N, (i, state) => {
+                Real = Imag = 0;
+
+                dft[i] = new Complex(0, 0);
+
+                for (k = 0; k < N; k++)
                 {
-                    Real += arr[k] * Math.Cos(pi * n * k / N);
-                    Imag -= arr[k] * Math.Cos(pi * n * k / N);
+                    Real += data[i] * Math.Cos(2 * pi * i * k / N);
+                    Imag -= data[i] * Math.Sin(2 * pi * i * k / N);
                 }
 
-                dft[n].real = Real;
-                dft[n].imag = Imag;
-            }
-            
-
-            return dft;
-        }
-
-
-        static public Complex[] optimized_DFT(double[] arr)
-        {
-            int q = (int)Math.Log(arr.Length, 2);
-            int N = (int)Math.Pow(2, q);
-            int N4 = N / 4, N2 = N / 2, N34 = 3 * N / 4;
-
-            Complex[] dft = new Complex[N];
-            int n = 0, k = 0;
-            double coef = 0.25 * Math.PI / N;
-            double coef_n = 0;
-            double sqr2 = Math.Sqrt(2);
-            double Real = 0, Imag = 0;
-
-            var ForDFT = Parallel.For(0, N, (i, state) => {
-            Real = Imag = 0;
-            dft[n] = new Complex(0, 0);
-            coef_n = coef * n;
-            for (k = 0; k < Math.Pow(2, q - 2); k++)
-            {
-                Real += arr[k] * sqr2 * Math.Cos(coef_n * (4 * k - N)) + arr[k + N4] * Math.Cos(coef_n * (4 * k + N));
-                Real += arr[k + N2] * Math.Cos(2 * coef_n * (2 * k + N)) + arr[k + N34] * Math.Cos(coef_n * (4 * k + 3 * N));
-
-                Imag -= arr[k] * sqr2 * Math.Sin(coef_n * (4 * k + 3 * N)) + arr[k + N4] * Math.Sin(coef_n * (4 * k + N));
-                Imag -= arr[k + N2] * Math.Sin(2 * coef_n * (2 * k + N)) + arr[k + N34] * Math.Sin(coef_n * (4 * k + 3 * N));
-            }
-
-                dft[n].real = Real;
-                dft[n].imag = Imag;
+                dft[i].real = Real;
+                dft[i].imag = Imag;
             });
 
             return dft;
         }
 
+        static public double[,] optimized_DFT(double[] arr, int l, int a=0)
+        {
+            double[,] dft = new double[l, 2];
+            int q = (int)Math.Log(l, 2);
+            int q1 = q / 2;
+            int q2 = q - q1;
+            int p = (int)(Math.Pow(2, q2));
+            int s = (int)(Math.Pow(2, q1));
+            double A = 0, B = 0;
+
+            var ForDFT = Parallel.For(0, l, (k, state) => {
+                dft[k, 0] = 0;
+                dft[k, 1] = 0;
+
+                for (int u=0; u<s; u+=4)
+                {
+                    A = 0;
+                    B = 0;
+
+                    for (int v = 0; v < p; v+=4)
+                    {
+                        A = 0.5 * (arr[a + (s - 1) * v + u] + arr[a + (s - 1) * v + u + 1]) * Math.Cos(2 * Math.PI * (s - 1) * v / l);
+                        B = 0.5 * (arr[a + (s - 1) * v + u] + arr[a + (s - 1) * v + u + 1]) * Math.Sin(2 * Math.PI * (s - 1) * v / l);
+                    }
+
+                    dft[k, 0] += A * Math.Cos(2 * Math.PI * k * u / l) - B * Math.Sin(2 * Math.PI * k * u / l);
+                    dft[k, 1] += B * Math.Cos(2 * Math.PI * k * u / l) + A * Math.Sin(2 * Math.PI * k * u / l);
+                }
+
+                dft[k, 0] /= l;
+                dft[k, 1] /= l;
+            });
+
+            return dft;
+        }
+
+        public static Complex[] FFT_Cooley_Tukey(double[] arr, int l, int a = 0)
+        {
+            double[] input = new double[l];
+            int i = 0;
+
+            for(i=0; i<l; i++)
+            {
+                input[i] = arr[i+a+l];
+            }
+
+            Complex[] data = new Complex[l];
+            for (i = 0; i<l; i++)
+            {
+                data[i] = new Complex(input[i], 0);
+            }
+
+            FFTRecursive(data);
+            return data;
+        }
+
+        private static void FFTRecursive(Complex[] data)
+        {
+            int n = data.Length;
+            if (n <= 1) return;
+
+            // Розбиття на парні та непарні індекси
+            Complex[] even = new Complex[n / 2];
+            Complex[] odd = new Complex[n / 2];
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                even[i] = data[2 * i];
+                odd[i] = data[2 * i + 1];
+            }
+
+            FFTRecursive(even);
+            FFTRecursive(odd);
+
+            var ForDFT = Parallel.For(0, n/2, (k, state) => {
+                Complex t = Complex.Exp(new Complex(-2 * Math.PI * odd[k].imag * k / n, 0)) * odd[k];
+                data[k] = even[k] + t;
+                data[k + n / 2] = even[k] - t;
+            });
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace DFT_optimize
 {
@@ -11,13 +13,78 @@ namespace DFT_optimize
         static void Main(string[] args)
         {
             String str;
-            int N = 11525, i=0;
-            var rand = new Random();
-            double max_A = 10;
+            Stopwatch stopwatch = new Stopwatch();
+            int N = 0, i=0;
+            int l = 1024, step = 256;
+            int dl = 2 * l;
+            int acc = 1;
 
-            double[] arr = new double[N]; // random signal
-            var dft = DFT.Calc_DFT(arr); // usual DFT
-            var mdft = DFT.optimized_DFT(arr); // optimized DFT
+            // data reading
+            N = File.ReadAllLines("data/channel0.txt").Length;
+            double[,] samples = new double[4, N];
+
+            for (int channel = 0; channel < 4; channel++)
+            {
+                String[] signal_str_values = File.ReadAllLines($"data/channel{channel}.txt");
+                N = signal_str_values.Length;
+                double[] arr = new double[N];
+                for (i = 0; i < N; i++)
+                {
+                    Double.TryParse(signal_str_values[i], out samples[channel, i]);
+                }
+                
+            }
+
+            Console.WriteLine("Testing DFT");
+            stopwatch.Start();
+            for (int channel=0; channel<4; channel++)
+            {
+                double[] arr = new double[N];
+                for (i = 0; i < N; i++)
+                {
+                    arr[i] = samples[channel, i];
+                }
+                for(int a=0; a<N; a+=step)
+                {
+                    var dft = DFT.Calc_DFT(arr, dl * acc, a); // usual DFT
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"DFT needed {stopwatch.ElapsedMilliseconds} milliseconds");
+
+            Console.WriteLine("Testing modifide DFT");
+            stopwatch.Start();
+            for (int channel = 0; channel < 4; channel++)
+            {
+                double[] arr = new double[N];
+                for (i = 0; i < N; i++)
+                {
+                    arr[i] = samples[channel, i];
+                }
+                for (int a = 0; a < N; a += step)
+                {
+                    var dft = DFT.optimized_DFT(arr, dl * acc, a); // optimized DFT
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Modified DFT needed {stopwatch.ElapsedMilliseconds} milliseconds");
+
+            Console.WriteLine("Testing FFT by Cooley-Tuley method");
+            stopwatch.Start();
+            for (int channel = 0; channel < 4; channel++)
+            {
+                double[] arr = new double[N];
+                for (i = 0; i < N; i++)
+                {
+                    arr[i] = samples[channel, i];
+                }
+                for (int a = 0; a < N; a += step)
+                {
+                    var dft = DFT.FFT_Cooley_Tukey(arr, dl * acc, a); // Cooley-Tuley method
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Modified DFT needed {stopwatch.ElapsedMilliseconds} milliseconds");
 
             str = Console.ReadLine();
         }
